6ab28116748712bf7d2c7f1bc72810c4c49ba54b
dethMastery <owner@detzz.in.th>
2022-05-11 03:32:54 +0700

    added: docs page

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ let app = exp()
 let wPort = 8090
 
 app.set('views', path.join(__dirname, 'views'))
+app.use(exp.static(__dirname + '/public'))
 app.set('view engine', 'jade')
 
 function homePage(req, res) {
     res.render('index.jade')
 }
 
+function docs(req, res) {
+    res.render('docs.jade')
+}
+
 app.get('/', homePage)
+app.get('/docs', docs)
 
 app.listen(wPort, () => {
     console.log('Express starting @ http://localhost:' + wPort);
