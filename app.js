08d90a313677f88e1575bf0130ef5d97c76e46dc
dethMastery <owner@detzz.in.th>
2022-05-11 03:35:47 +0700

    added: cors

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 let exp = require('express')
 let path = require('path')
+let cors = require('cors')
 let app = exp()
 let wPort = 8090
 
 app.set('views', path.join(__dirname, 'views'))
 app.use(exp.static(__dirname + '/public'))
 app.set('view engine', 'jade')
+
+app.use(cors())
+
+let forFree = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 function homePage(req, res) {
     res.render('index.jade')
